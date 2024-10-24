48aa59012a1ccf1ed98cb6ccb75239dc272bdfa4
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2024-10-24 17:45:46 +0000

    Add new pipeline, dataset, job, and gem for Project-Automationchetan scala release API.

---
+++ pipelines/Pipeline-Automationchetan_RELEASE_API_1729791449056/code/src/main/scala/io/prophecy/pipelines/pipelineautomationchetan_release_api_1729791449056/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.pipelineautomationchetan_release_api_1729791449056.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
---
+++ pipelines/Pipeline-Automationchetan_RELEASE_API_1729791449056/code/src/main/scala/io/prophecy/pipelines/pipelineautomationchetan_release_api_1729791449056/main.scala
@@ -0,0 +1,49 @@
+package io.prophecy.pipelines.pipelineautomationchetan_release_api_1729791449056
+
+import io.prophecy.libs._
+import io.prophecy.pipelines.pipelineautomationchetan_release_api_1729791449056.config._
+import io.prophecy.pipelines.pipelineautomationchetan_release_api_1729791449056.functions.UDFs._
+import io.prophecy.pipelines.pipelineautomationchetan_release_api_1729791449056.functions.PipelineInitCode._
+import io.prophecy.pipelines.pipelineautomationchetan_release_api_1729791449056.graph._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.expressions._
+import java.time._
+
+object Main {
+
+  def apply(context: Context): Unit = {
+    val df_Project_Automationchetan_scala_RELEASE_API_1729791449056_dataSet =
+      Project_Automationchetan_scala_RELEASE_API_1729791449056_dataSet(context)
+    val df_customer_details_projection = customer_details_projection(
+      context,
+      df_Project_Automationchetan_scala_RELEASE_API_1729791449056_dataSet
+    )
+  }
+
+  def main(args: Array[String]): Unit = {
+    val config = ConfigurationFactoryImpl.getConfig(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Pipeline-Automationchetan_RELEASE_API_1729791449056")
+      .config("spark.default.parallelism",             "4")
+      .config("spark.sql.legacy.allowUntypedScalaUDF", "true")
+      .enableHiveSupport()
+      .getOrCreate()
+    val context = Context(spark, config)
+    spark.conf.set(
+      "prophecy.metadata.pipeline.uri",
+      "pipelines/Pipeline-Automationchetan_RELEASE_API_1729791449056"
+    )
+    registerUDFs(spark)
+    MetricsCollector.instrument(
+      spark,
+      "pipelines/Pipeline-Automationchetan_RELEASE_API_1729791449056"
+    ) {
+      apply(context)
+    }
+  }
+
+}
