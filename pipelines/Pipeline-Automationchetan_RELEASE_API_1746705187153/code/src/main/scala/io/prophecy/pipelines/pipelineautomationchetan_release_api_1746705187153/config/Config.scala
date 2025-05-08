dfed67d2182a53008a1f211f552b17c7cdeb0bc1
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2025-05-08 12:03:42 +0000

    Add new components for Project Automation: pipeline, dataset, job, and gem

---
+++ pipelines/Pipeline-Automationchetan_RELEASE_API_1746705187153/code/src/main/scala/io/prophecy/pipelines/pipelineautomationchetan_release_api_1746705187153/config/Config.scala
@@ -0,0 +1,6 @@
+package io.prophecy.pipelines.pipelineautomationchetan_release_api_1746705187153.config
+
+import pureconfig._
+import pureconfig.generic.ProductHint
+import io.prophecy.libs._
+case class Config() extends ConfigBase
---
+++ pipelines/Pipeline-Automationchetan_RELEASE_API_1746705187153/code/src/main/scala/io/prophecy/pipelines/pipelineautomationchetan_release_api_1746705187153/config/Context.scala
@@ -0,0 +1,4 @@
+package io.prophecy.pipelines.pipelineautomationchetan_release_api_1746705187153.config
+
+import org.apache.spark.sql.SparkSession
+case class Context(spark: SparkSession, config: Config)
